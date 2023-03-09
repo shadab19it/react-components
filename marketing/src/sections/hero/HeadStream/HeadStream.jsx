b972a6e9320151271ca8126d41c7792415ab88f5
Shad <shad.amezng@gmail.com>
2023-03-09 12:45:45 +0530

    added head stream

---
+++ marketing/src/sections/hero/HeadStream/HeadStream.jsx
@@ -0,0 +1,37 @@
+import "./HeadStream.css";
+
+const HeadStream = () => (
+  <section className='section-headstream'>
+    <div>
+      <h1>Get ahead of competition</h1>
+      <p>
+        With Hundred.dev automate your mundate tasks, <br /> and build technology that can help <br /> you move faster than ever
+      </p>
+      <button>Get Started</button>
+    </div>
+    <div>
+      <div className='stream-container'>
+        <div className='circle' style={{ backgroundColor: "orange" }}></div>
+        <img src='/img/sections/hero/coder_4.jpeg' alt='' />
+      </div>
+      <div className='stream-container'>
+        <img src='/img/sections/hero/coder_5.png' alt='' />
+        <div className='cylinder' style={{ backgroundColor: "orange" }}></div>
+      </div>
+      <div className='stream-container'>
+        <div className='cylinder' style={{ backgroundColor: "#628aec" }}></div>
+        <img src='/img/sections/hero/coder_3.jpeg' alt='' />
+        <img src='/img/sections/hero/coder_1.jpeg' alt='' />
+      </div>
+      <div className='stream-container'>
+        <img src='/img/sections/hero/woman.jpeg' alt='' />
+        <div className='cylinder' style={{ backgroundColor: "#ea7e60" }}></div>
+      </div>
+      <div className='stream-container'>
+        <img src='/img/sections/hero/coding.jpeg' alt='' />
+      </div>
+    </div>
+  </section>
+);
+
+export default HeadStream;
