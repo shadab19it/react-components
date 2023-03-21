035e92e3422233d18510ee4ffa90d36daf0e9979
Shadab Alam <shadab19it@gmail.com>
2023-03-21 12:11:09 +0530

    move svg icon to public/img/svg folder

--- marketing/src/index.js
+++ marketing/src/index.js
@@ -1,15 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-// import App from "./App";
 import reportWebVitals from "./reportWebVitals";
-// import AlloyNav from "./sections/navigation/AlloyNav/AlloyNav";
-// import HeadStream from "./sections/hero/HeadStream/HeadStream";
-// import BlueSimple from "./sections/navigation/Blue-Simple/Blue-Simple";
-// import FullWidthDropdown from "./sections/navigation/FullWidthDropdown/FullWidthDropdown";
+import SpotAINav from "./sections/navigation/SpotAINav/SpotAINav";
+import FullWidthDropdown from "./sections/navigation/FullWidthDropdown/FullWidthDropdown";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<React.StrictMode></React.StrictMode>);
+root.render(
+  <React.StrictMode>
+    <SpotAINav />
+  </React.StrictMode>
+);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
