473bbab4eaf2d96f45a67d7e995990173f2a9f70
Shadab Alam <shadab19it@gmail.com>
2023-03-22 11:38:51 +0530

    remove import un-rendered components

--- marketing/src/index.js
+++ marketing/src/index.js
@@ -1,12 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-// import App from "./App";
 import reportWebVitals from "./reportWebVitals";
-// import AlloyNav from "./sections/navigation/AlloyNav/AlloyNav";
-// import HeadStream from "./sections/hero/HeadStream/HeadStream";
-// import BlueSimple from "./sections/navigation/Blue-Simple/Blue-Simple";
-// import FullWidthDropdown from "./sections/navigation/FullWidthDropdown/FullWidthDropdown";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(<React.StrictMode></React.StrictMode>);
