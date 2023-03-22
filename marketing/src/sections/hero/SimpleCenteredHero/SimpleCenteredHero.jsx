7b5c16ba744a93d91272c3d2f1c44abae19aec70
Shadab Alam <shadab19it@gmail.com>
2023-03-22 14:07:50 +0530

    added *.module.css style

--- marketing/src/sections/hero/SimpleCenteredHero/SimpleCenteredHero.jsx
+++ marketing/src/sections/hero/SimpleCenteredHero/SimpleCenteredHero.jsx
@@ -1,22 +1,25 @@
 import React from "react";
-import style from "./SimpleCenteredHero.module.css";
+import styles from "./SimpleCenteredHero.module.css";
 
 const SimpleCenteredHero = () => {
   return (
-    <div>
-      <h1>
-        Everything you need in <span>one place</span>
+    <div className={styles.simple_centered_hero}>
+      <h1 className={styles.hero_title}>
+        Everything you need in <span className={styles.hero_highlight_word}>one place</span>
       </h1>
-      <p>
+      <p className={styles.hero_desc}>
         Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quae consequuntur id commodi, tempora minus ipsam adipisci distinctio
         expedita voluptas dolore.
       </p>
-      <div>
+      <div className={styles.hero_action_btns}>
         <button>Get Started</button>
-        <button>Learn more</button>
+        <button>
+          <span>Learn more</span>
+          <img src='/img/sections/hero/simpleCenteredHero/caret-right.svg' alt='' />
+        </button>
       </div>
 
-      <div>
+      <div className={styles.hero_bottom_img}>
         <img src='' alt='' />
       </div>
     </div>
