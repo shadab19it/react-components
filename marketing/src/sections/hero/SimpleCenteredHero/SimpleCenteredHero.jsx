1b9181cdd60b33b111f5b8153faa4e67f9dbdc21
Shadab Alam <shadab19it@gmail.com>
2023-03-22 12:32:25 +0530

    added simple centered hero section

---
+++ marketing/src/sections/hero/SimpleCenteredHero/SimpleCenteredHero.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+import style from "./SimpleCenteredHero.module.css";
+
+const SimpleCenteredHero = () => {
+  return (
+    <div>
+      <h1>
+        Everything you need in <span>one place</span>
+      </h1>
+      <p>
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quae consequuntur id commodi, tempora minus ipsam adipisci distinctio
+        expedita voluptas dolore.
+      </p>
+      <div>
+        <button>Get Started</button>
+        <button>Learn more</button>
+      </div>
+
+      <div>
+        <img src='' alt='' />
+      </div>
+    </div>
+  );
+};
+
+export default SimpleCenteredHero;
